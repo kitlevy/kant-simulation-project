f104ba76d4a7a14080164c93c6da1a2cb63ee228
Kit Levy <levyec@bc.edu>
2025-01-19 20:22:02 -0500

    first try at physics

--- interacting_balls.py
+++ interacting_balls.py
@@ -1,54 +1,81 @@
 import pygame
 import numpy as np
 
-#Ball class w/ wall collisions, other ball collisions
+#Ball class w/ wall collisions,other ball collisions
 class Ball:
 
     def __init__(self,x,y,radius,dx,dy,color):
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
-        self.color = color
-        self.rad = radius
+        self.x=x
+        self.y=y
+        self.dx=dx
+        self.dy=dy
+        self.color=color
+        self.rad=radius
+        self.mass=np.pi*(self.rad**2)
+    
+    def setvelo(self,newdx,newdy):
+        self.dx=newdx
+        self.dy=newdy
     
     def display(self):
         self.x+=self.dx
         self.y+=self.dy
 
-        if self.x - self.rad <= 0 or self.x + self.rad >= width:
-            self.dx = -self.dx
-        if self.y - self.rad <= 0 or self.y + self.rad >= height:
-            self.dy = -self.dy
+        #wall collisions
+        if self.x-self.rad<=0 or self.x+self.rad>=width:
+            self.dx=-self.dx
+        if self.y-self.rad<=0 or self.y+self.rad>=height:
+            self.dy=-self.dy
         
+        #ball-to-ball collisions
         for ball in balls:
-            if ball != self:
-                d = np.sqrt((self.x-ball.x)**2+(self.y-ball.y)**2)
+            if ball!=self:
+                d=np.sqrt((self.x-ball.x)**2+(self.y-ball.y)**2)
                 if d<=self.rad+ball.rad:
-                    self.dx = -self.dx
-                    self.dy = -self.dy
+                    #using that calc 3 + high school physics knowledge
+                    p1=np.array([self.x,self.y])
+                    p2=np.array([ball.x,ball.y])
+                    v1=np.array([self.dx,self.dy])
+                    v2=np.array([ball.dx,ball.dy])
+
+                    r=p1-p2
+                    n=r/np.sqrt(r.dot(r))
+                    
+                    relv=v1-v2
+                    imp=2*(relv.dot(n))*(self.mass*ball.mass)/(self.mass+ball.mass)
+                    
+                    newv1=v1-(imp/self.mass)*n
+                    newv2=v2+(imp/ball.mass)*n
+
+                    self.setvelo(newv1[0],newv1[1])
+                    ball.setvelo(newv2[0],newv2[1])
+
+
+
         
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.rad)
+        pygame.draw.circle(screen,self.color,(self.x,self.y),self.rad)
 
 
 pygame.init()
 
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-white = (255, 255, 255)
-blue = (173, 216, 230)
+width,height=800,600
+screen=pygame.display.set_mode((width,height))
+white=(255,255,255)
+blue=(173,216,230)
 
 balls=[]
-balls.append(Ball(100,100,20,np.random.choice([-4,4]),np.random.choice([-4,4]),blue))
+balls.append(Ball(100,100,40,np.random.choice([-4,4]),np.random.choice([-4,4]),blue))
 balls.append(Ball(150,150,20,np.random.choice([-4,4]),np.random.choice([-4,4]),blue))
 balls.append(Ball(200,200,20,np.random.choice([-4,4]),np.random.choice([-4,4]),blue))
+balls.append(Ball(150,350,20,np.random.choice([-4,4]),np.random.choice([-4,4]),blue))
+balls.append(Ball(400,200,20,np.random.choice([-4,4]),np.random.choice([-4,4]),blue))
 
 #main pygame loop
-running = True
+running=True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running=False
 
     screen.fill(white)
 
